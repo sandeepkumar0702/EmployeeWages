2550071fdb7dd09bbd30a4ed934ffa348bc50b15
Sandeep Kumar <sandeepkumar073113@gmail.com>
2024-12-04 14:37:01 +0530

    Implemeneted use case2

--- Employee.js
+++ Employee.js
@@ -1,7 +1,31 @@
 const presentOrAbsent=Math.random();
+let workHour;
+let salary;
 if(presentOrAbsent<0.5){
+    workHour=0;
+    salary=0;
     console.log("Absent");
 }
 else{
     console.log("Present");
+    const salaryPerHour=20;
+    let hour=Math.floor(Math.random()*2)+1;
+    switch(hour){
+        case 1:{
+            workHour=4;
+            salary=workHour*salaryPerHour;
+            break;
+        }
+        case 2:{
+            workHour=8;
+            salary=workHour*salaryPerHour;
+            break;
+        }
+        default:{
+            workHour=0;
+            salary=workHour*salaryPerHour;
+            break;
+        }
+    }
 }
+console.log(salary);
